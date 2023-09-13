ef56c28c22056e24358cd3dd8bf2cfccccf37e53
VagapovMI <vgapov02@gmail.com>
2023-09-13 12:30:08 +0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+  <header class="Mainheader" > Пройдите опрос клиента сети магазинов МегаХенд </header>
+</div>
+</template>
+
+<style scoped>
+.Mainheader {
+  text-align: center;
+  vertical-align: middle;
+  margin-top: 0rem;
+}
+
+</style>
